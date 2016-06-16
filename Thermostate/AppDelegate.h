5dfaf35433b54acd1af269298dc140cbc596c30e
tolyasjar <toleenj@gmail.com>
2016-06-16 12:15:22 -0500

    Initial Commit

---
+++ Thermostate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thermostate
+//
+//  Created by Toleen Jaradat on 6/16/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Thermostate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thermostate
+//
+//  Created by Toleen Jaradat on 6/16/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
